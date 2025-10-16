df1447d5375951da0ee940795b5b63a05365cb2a
mbany <mbany@student.42.pl>
2025-10-16 19:53:04 +0200

    push all files

---
+++ js/header.js
@@ -0,0 +1,163 @@
+var slideshowSpeed = 4000;
+
+var photos = [ {
+		"title" : "",
+		"image" : "H_Zimny_kat2.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	}, {
+		"title" : "",
+		"image" : "H_warnolty2.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	}, {
+		"title" : "",
+		"image" : "H_sniardwy_1.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	}, {
+		"title" : "",
+		"image" : "H_Zimny_kat3.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	},{
+		"title" : "",
+		"image" : "H_ocean1.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	},{
+		"title" : "",
+		"image" : "H_stpierr.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	},{
+		"title" : "",
+		"image" : "H_NaviBHeader.jpg",
+		"url" : "",
+		"firstline" : "",
+		"secondline" : ""
+	}		
+];
+
+
+
+$(document).ready(function() {
+		
+	$("#back").click(function() {
+		stopAnimation();
+		navigate("back");
+	});
+	
+	$("#next").click(function() {
+		stopAnimation();
+		navigate("next");
+	});
+	
+	var interval;
+	$("#controls").toggle(function(){
+		stopAnimation();
+	}, function() {
+		
+		navigate("next");
+		
+		interval = setInterval(function() {
+			navigate("next");
+		}, slideshowSpeed);
+	});
+	
+	
+	var activeContainer = 1;	
+	var currentImg = 0;
+	var animating = false;
+	var navigate = function(direction) {
+		// Check if no animation is running. If it is, prevent the action
+		if(animating) {
+			return;
+		}
+		
+		// Check which current image we need to show
+		if(direction == "next") {
+			currentImg++;
+			if(currentImg == photos.length + 1) {
+				currentImg = 1;
+			}
+		} else {
+			currentImg--;
+			if(currentImg == 0) {
+				currentImg = photos.length;
+			}
+		}
+		
+		// Check which container we need to use
+		var currentContainer = activeContainer;
+		if(activeContainer == 1) {
+			activeContainer = 2;
+		} else {
+			activeContainer = 1;
+		}
+		
+		showImage(photos[currentImg - 1], currentContainer, activeContainer);
+		
+	};
+	
+	var currentZindex = -1;
+	var showImage = function(photoObject, currentContainer, activeContainer) {
+		animating = true;
+		
+		// Make sure the new container is always on the background
+		currentZindex--;
+		
+		// Set the background image of the new active container
+
+		$("#headerimg" + activeContainer).css({
+			"background-image" : "url(../photo/" + photoObject.image + ")",
+			"display" : "block",
+			"z-index" : currentZindex
+		});
+		
+		// Hide the header text
+		$("#headertxt").css({"display" : "none"});
+		
+		// Set the new header text
+		$("#firstline").html(photoObject.firstline);
+		$("#secondline")
+			.attr("href", photoObject.url)
+			.html(photoObject.secondline);
+		$("#pictureduri")
+			.attr("href", photoObject.url)
+			.html(photoObject.title);
+		
+		
+		// Fade out the current container
+		// and display the header text when animation is complete
+		$("#headerimg" + currentContainer).fadeOut(function() {
+			setTimeout(function() {
+				$("#headertxt").css({"display" : "block"});
+				animating = false;
+			}, 200);
+		});
+	};
+	
+	var stopAnimation = function() {
+		// Change the background image to "play"
+		$("#control").css({ "background-image" : "" });//url(/layouts/unitechem/img/btn_play.png)//
+		
+		// Clear the interval
+		clearInterval(interval);
+	};
+	
+	// We should statically set the first image
+	navigate("next");
+	
+	// Start playing the animation
+	interval = setInterval(function() {
+		navigate("next");
+	}, slideshowSpeed);
+	
+});
---
+++ js/maps.js
@@ -0,0 +1,67 @@
+var geocoder;
+var map;
+var dymek = new google.maps.InfoWindow({maxWidth:'250'});
+
+// wspólne cechy ikon
+var rozmiar = new google.maps.Size(24, 30);
+var rozmiar_cien = new google.maps.Size(12, 8);
+var punkt_startowy = new google.maps.Point(0, 0);
+var punkt_zaczepienia = new google.maps.Point(16, 16);
+
+function initialize(){
+   geocoder = new google.maps.Geocoder();
+   var myOptions = {
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+       //mapTypeId: google.maps.MapTypeId.TERRAIN,
+       mapTypeControl: true
+   }
+ map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+				
+}
+
+
+
+function showGPS(gpse, gpsn, txt){
+    var wspolrzedne = new google.maps.LatLng(gpse, gpsn);
+    map.setCenter(wspolrzedne);
+    ikona = new google.maps.MarkerImage("../images/sail.png", rozmiar, punkt_startowy, punkt_zaczepienia); 
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: ikona,
+        position: wspolrzedne
+    });
+    
+    google.maps.event.addListener(marker, "click", function(){
+        //dymek.setContent(txt);
+        //dymek.open(map, marker);
+    });
+    
+    google.maps.event.trigger(marker, 'click');
+    
+}
+
+function showGPS2(gpse, gpsn, txt){
+    var wspolrzedne = new google.maps.LatLng(gpse, gpsn);
+    var marker = new google.maps.Marker({
+        map: map,
+		icon: '../images/flag12z.png',
+        position: wspolrzedne,
+		
+    });
+    
+    google.maps.event.addListener(marker, "mouseover", function(){
+		dymek.setContent(txt);
+        dymek.open(map, marker);
+    });
+    
+	google.maps.event.addListener(marker, "mouseout", function(){
+        //dymek.setContent(txt);
+        dymek.close();
+    });
+}
+
+// ta funkcja generuje zawartość okienka informacyjnego
+//function zawartoscOkna(tytul, opis){
+//    return '<h3>' + tytul + '</h3><p>' + opis + '</p>';
+//}
